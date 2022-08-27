739a4a3ac1641f8b50adb843cb966179b804a546
Radiit <74106446+Radiit@users.noreply.github.com>
2022-08-27 20:35:45 +0700

    added status field

--- CRUD/models.py
+++ CRUD/models.py
@@ -4,6 +4,7 @@ class kegiatan(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField()
     created_at = models.DateTimeField()
+    status = models.BooleanField(default=True)
 
     def __str__(self) -> str:
         return self.name
